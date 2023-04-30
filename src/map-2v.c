c5e3a2d8305a556c70bba4cfdf77149cb3437154
Afonsos4ntos <afonso.ds33@gmail.com>
2023-04-30 18:08:27 +0100

    Light sys

--- src/map-2v.c
+++ src/map-2v.c
@@ -8,7 +8,7 @@
 
 
 int max_x, max_y;
-char **mapa;
+char **map;
 
 void define() {
     // Inicializa a biblioteca ncurses
@@ -18,27 +18,28 @@ void define() {
     max_y = getmaxy(stdscr);
     max_x = getmaxx(stdscr);
 
-    mapa = malloc(sizeof(char*) * max_x);
+    map = malloc(sizeof(char*) * max_x);
     for (int i = 0; i < max_x; i++) {
-        mapa[i] = malloc(sizeof(char) * max_y);
+        map[i] = malloc(sizeof(char) * max_y);
     }
 }
 
 
-// #define ALTURA  49
-// #define COMPRIMENTO 180
-#define ALTURA (max_y-7)
-#define COMPRIMENTO (max_x-58)
+// #define HEIGHT  49
+// #define LENGTH 180
+#define HEIGHT (max_y-7)
+#define LENGTH (max_x-58)
     
 #define player_char '@'
-#define wall '#'
-#define floor '#'
-#define boox '='
-#define empty ' '
-#define parede '.'
+#define edge '#'
 
-// char mapa[COMPRIMENTO][ALTURA];
-int tamanho = 5;
+#define boox '='
+#define empty '.'
+#define obstacle '+'
+
+
+// char map[LENGTH][HEIGHT];
+int SIZE = 5;
 
 #define Shapes 3
 
@@ -48,18 +49,29 @@ typedef struct Position {
     int y;
 } Position;
 
+typedef struct inventory{
+    //armas
+    int flashlight_radius;
+}inventory;
+
+
 typedef struct Player{
     Position pos ;
     //int health;    
     //int attack;
+    inventory inventory;
 }player;
 
 
+
+
+
+
 int check(int x, int y) {
-    if (x < 2 || x >= COMPRIMENTO - 2|| y < 2 || y >= ALTURA - 2) {
+    if (x < 2 || x >= LENGTH - 2|| y < 2 || y >= HEIGHT - 2) {
         return 0;
     }
-    if (mapa[x][y] != wall && mapa[x][y] != floor) {
+    if (map[x][y] != edge ) {
         return 1;
     } else {
         return 0;
@@ -68,10 +80,11 @@ int check(int x, int y) {
 
 
 int check_player(int x, int y) {
-    if (x <= 2 || x >= COMPRIMENTO - 2|| y <= 2 || y >= ALTURA - 2) {
+    if (x <= 2 || x >= LENGTH - 2|| y <= 2 || y >= HEIGHT - 2) {
         return 0;
     }
-    if (mapa[x][y] == empty && x>2 && x<COMPRIMENTO-2 && y>2 && y<ALTURA-2) {
+    if (map[x][y] == empty && x>2 && x<LENGTH-2 && y>2 && y<HEIGHT-2) {
+        
         return 1;
     } else {
         return 0;
@@ -81,15 +94,15 @@ int check_player(int x, int y) {
 
 
 
-void gera_Triangle(int tamanho, Position pos)
+void creat_Triangle(int SIZE, Position pos)
 {
     int i, j;
-    for (i = 0; i < tamanho; i++) {
-        for (j = 0; j < tamanho - i; j++) {
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE - i; j++) {
 
             if (check(pos.x+j,pos.y+i)) {
 
-                mapa[pos.x + j][pos.y + i] = parede;
+                map[pos.x + j][pos.y + i] = obstacle;
             }
             
         }
@@ -98,14 +111,14 @@ void gera_Triangle(int tamanho, Position pos)
 }
 
 
-void gera_Square(int tamanho,Position pos)
+void creat_Square(int SIZE,Position pos)
 {   
-    for (int i=0; i<tamanho; i++) {
-        for (int j=0;j<tamanho ; j++) {
+    for (int i=0; i<SIZE; i++) {
+        for (int j=0;j<SIZE ; j++) {
             
             if (check(pos.x+j,pos.y+i)) {
                 
-                mapa[pos.x + j][pos.y + i] = parede;
+                map[pos.x + j][pos.y + i] = obstacle;
             }
        }
     }
@@ -113,39 +126,39 @@ void gera_Square(int tamanho,Position pos)
 }
 
 
-//mapa[pos.x + x][pos.y + y - i]
-void gera_Circle(int tamanho, Position pos)
+
+void creat_Circle(int SIZE, Position pos)
 {
     int x = 0;
-    int y = tamanho;
-    int d = 3 - 2 * tamanho;
-    int densidade = 6; // Ajuste o valor de densidade para aumentar ou diminuir a densidade do círculo
+    int y = SIZE;
+    int d = 3 - 2 * SIZE;
+    int density = SIZE+1; // Ajuste o valor de density para aumentar ou diminuir a density do círculo
 
     while (x <= y) {
-        for (int i = 0; i < densidade; i++) {
+        for (int i = 0; i < density; i++) {
             if (check(pos.x+x,pos.y+y-i)) {
-                mapa[pos.x + x][pos.y + y - i] = parede;
+                map[pos.x + x][pos.y + y - i] = obstacle;
             }
             if (check(pos.x-x,pos.y+y-i)) {
-                mapa[pos.x - x][pos.y + y - i] = parede;
+                map[pos.x - x][pos.y + y - i] = obstacle;
             }
             if (check(pos.x+x,pos.y-y+i)) {
-                mapa[pos.x + x][pos.y - y + i] = parede;
+                map[pos.x + x][pos.y - y + i] = obstacle;
             }
             if (check(pos.x-x,pos.y-y+i)) {
-                mapa[pos.x - x][pos.y - y + i] = parede;
+                map[pos.x - x][pos.y - y + i] = obstacle;
             }
             if (check(pos.x+y-i,pos.y+x)) {
-                mapa[pos.x + y - i][pos.y + x] = parede;
+                map[pos.x + y - i][pos.y + x] = obstacle;
             }
             if (check(pos.x-y+i,pos.y+x)) {
-                mapa[pos.x - y + i][pos.y + x] = parede;
+                map[pos.x - y + i][pos.y + x] = obstacle;
             }
             if (check(pos.x+y-i,pos.y-x)) {
-                mapa[pos.x + y - i][pos.y - x] = parede;
+                map[pos.x + y - i][pos.y - x] = obstacle;
             }
             if (check(pos.x-y+i,pos.y-x)) {
-                mapa[pos.x - y + i][pos.y - x] = parede;
+                map[pos.x - y + i][pos.y - x] = obstacle;
             }
         }
 
@@ -160,28 +173,28 @@ void gera_Circle(int tamanho, Position pos)
     }
 }
 
-void gera_wall()
+void creat_edge()
 {
     srand(time(0));
 
-    int num_paredes = rand() % 11 + 37
+    int num_obstacles = rand() % 11 + 37
 ; 
 
-    for (int i = 0; i < num_paredes; i++) {
+    for (int i = 0; i < num_obstacles; i++) {
         int shape = rand() % 3; // Gere um tipo de forma aleatório (0 a 2)
         Position pos;
-        pos.x = rand() % (COMPRIMENTO - 2);
-        pos.y = rand() % (ALTURA - 2);
+        pos.x = rand() % (LENGTH - 2);
+        pos.y = rand() % (HEIGHT - 2);
 
         switch (shape) {
             case TRIANGLE:
-                gera_Triangle(tamanho, pos);
+                creat_Triangle(SIZE, pos);
                 break;
             case SQUARE:
-                gera_Square(tamanho, pos);
+                creat_Square(SIZE, pos);
                 break;
             case CIRCLE:
-                gera_Circle(tamanho, pos);
+                creat_Circle(SIZE, pos);
                 break;
             default:
                 break;
@@ -190,102 +203,153 @@ void gera_wall()
 }
 
 
-void gera_barril()
+void creat_barrel()
 {
     srand(time(0));
     int num_barris = rand() % 16 +35; 
 
     for (int i = 0; i < num_barris; i++) {
-        int x = rand() % (COMPRIMENTO - 2) ; 
-        int y = rand() % (ALTURA - 2) ;
+        int x = rand() % (LENGTH - 2) ; 
+        int y = rand() % (HEIGHT - 2) ;
 
-        if (mapa[x][y] == empty) {
-            mapa[x][y] = boox;
+        if (map[x][y] == empty) {
+            map[x][y] = boox;
         }
     }
 }
 
-void gera_mapa()
+void creat_map()
 {
-    for (int i =0; i<ALTURA; i++) {
-        for (int j =0;j<COMPRIMENTO; j++) {
-            if (j==0 ||j==COMPRIMENTO-1) {
+    for (int i =0; i<HEIGHT; i++) {
+        for (int j =0;j<LENGTH; j++) {
+            if (j==0 ||j==LENGTH-1 || i==0||i==HEIGHT-1) {
 
-                mapa[j][i]=wall;
+                map[j][i]=edge;
 
-            }else if (i==0||i==ALTURA-1) {
-
-                mapa[j][i] = floor;
             }else{
-                mapa[j][i]=empty;
+                map[j][i]=empty;
             }   
 
         }
 
     }
-
 }
+
+
 
 void print_map(player player)
 {   
     clear();
-    for (int i=0; i<ALTURA; i++) {
-        for (int j=0; j<COMPRIMENTO; j++) {  
-            if (i==player.pos.y && j==player.pos.x){
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < LENGTH; x++) {
+            int distance = sqrt(pow(y - player.pos.y, 2) + pow(x - player.pos.x, 2));
+            
+            if (y==0 ||y==HEIGHT-1 ||x==0 ||x==LENGTH-1 ) {
+                
+                attron(COLOR_PAIR(4)); // set color
+                mvprintw(y,x ,"%c" ,map[x][y]); // print cell
+                attroff( COLOR_PAIR(4)); // unset color
+                            
+            }else if (distance < player.inventory.flashlight_radius) {
 
-                mvprintw(i,j,"%c", player_char);               
-            }else {          
-                mvprintw(i,j,"%c",mapa[j][i]);
+                if (map[x][y]==empty) {
+
+                    attron(COLOR_PAIR(3)); // set color
+                    mvprintw(y,x ,"%c" ,map[x][y]); // print cell
+                    attroff( COLOR_PAIR(3)); // unset color
+
+                }else if (map[x][y]==boox) {
+                    
+                    attron(COLOR_PAIR(6)); // set color
+                    mvprintw(y,x ,"%c" ,map[x][y]); // print cell
+                    attroff( COLOR_PAIR(6)); // unset color
+                }else {
+
+                    attron(COLOR_PAIR(1)); // set color
+                    mvprintw(y,x ,"%c" ,map[x][y]); // print cell
+                    attroff( COLOR_PAIR(1)); // unset color                   
+                }
+
+
+            
+
+            } else {
+            
+                attron(COLOR_PAIR(2));
+                mvprintw(y,x,"%c" ,map[x][y]); // print black cell
+                attroff(COLOR_PAIR(2));
+            
             }
         }
     }
+    mvprintw(player.pos.y,player.pos.x,"%c",player_char);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+void free_map() {
+    // Libera memória alocada para o map
+    for (int i = 0; i < max_x; i++) {
+        free(map[i]);
+    }
+    free(map);
+}
+
+// ----------------------------------------------" movimento" ----------------------------------------
 
 void move_player(player player){
     while(1){
         int direction = getch();
         switch (direction) {
             case 'w':  // Cima
-                if (mapa[player.pos.x][player.pos.y-1] == empty) {
+                if (map[player.pos.x][player.pos.y-1] == empty ) {
                     player.pos.y--;
                 }
                 break;
             case 's':  // Baixo
-                if (mapa[player.pos.x][player.pos.y+1] == empty) {
+                if (map[player.pos.x][player.pos.y+1] == empty) {
                     player.pos.y++;
                 }
                 break;
             case 'a':  // Esquerda
-                if (mapa[player.pos.x-1][player.pos.y] == empty) {
+                if (map[player.pos.x-1][player.pos.y] == empty) {
                     player.pos.x--;
                 }
                 break;
             case 'd':  // Direita
-                if (mapa[player.pos.x+1][player.pos.y] == empty) {
+                if (map[player.pos.x+1][player.pos.y] == empty) {
                     player.pos.x++;
                 }
                 break;
             case '7':  // Diagonal superior esquerda
-                if (mapa[player.pos.x-1][player.pos.y-1] == empty) {
+                if (map[player.pos.x-1][player.pos.y-1] == empty) {
                     player.pos.x--;
                     player.pos.y--;
                 }
                 break;
             case '9':  // Diagonal superior direita
-                if (mapa[player.pos.x+1][player.pos.y-1] == empty) {
+                if (map[player.pos.x+1][player.pos.y-1] == empty) {
                     player.pos.x++;
                     player.pos.y--;
                 }
                 break;
             case '1':  // Diagonal inferior esquerda
-                if (mapa[player.pos.x-1][player.pos.y+1] == empty) {
+                if (map[player.pos.x-1][player.pos.y+1] == empty) {
                     player.pos.x--;
                     player.pos.y++;
                 }
                 break;
             case '3':  // Diagonal inferior direita
-                if (mapa[player.pos.x+1][player.pos.y+1] == empty) {
+                if (map[player.pos.x+1][player.pos.y+1] == empty) {
                     player.pos.x++;
                     player.pos.y++;
                 }
@@ -301,40 +365,47 @@ void move_player(player player){
 }
 
 
-
-
-
-void libera_mapa() {
-    // Libera memória alocada para o mapa
-    for (int i = 0; i < max_x; i++) {
-        free(mapa[i]);
-    }
-    free(mapa);
-}
-
-
-player gera_coordenadas() {
+player creat_coordinates() {
     player new_player;
     srand(time(NULL));
     while (check_player(new_player.pos.x, new_player.pos.y) != 1){
-        new_player.pos.x = (rand() % (COMPRIMENTO - 2));
-        new_player.pos.y = (rand() % (ALTURA - 2));
+        new_player.pos.x = (rand() % (LENGTH - 2));
+        new_player.pos.y = (rand() % (HEIGHT - 2));
     }
     return new_player;
 }
 
-
+//------------------------------------------- " movimento"---------------------------------------
 
 
 
 int main() {
     initscr(); // Inicializa a biblioteca ncurses
+    curs_set(0);
     define();
-    gera_mapa();
-    gera_wall();
-    gera_barril();
+    
+    start_color();
+   
+   
+    init_pair(1, COLOR_WHITE,  COLOR_BLACK);
+    init_pair(2, COLOR_BLACK,  COLOR_BLACK);
+    init_color(COLOR_YELLOW, 941, 902, 549);
+    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(4, COLOR_BLUE,   COLOR_BLACK);
+    
+    
+    init_color(COLOR_RED, 800, 400,0 );
+    init_pair(6, COLOR_RED,    COLOR_BLACK);
 
-    player new_player = gera_coordenadas(); // cria uma nova estrutura player com coordenadas aleatórias
+
+    curs_set(0);
+
+    creat_map();
+    creat_edge();
+    creat_barrel();
+
+    player new_player = creat_coordinates();
+    new_player.inventory.flashlight_radius=4; // cria uma nova estrutura player com coordenadas aleatórias
     print_map(new_player);
     move_player(new_player);
 
@@ -342,7 +413,7 @@ int main() {
 
     endwin(); // Encerra o uso da biblioteca ncurses
 
-    libera_mapa();
+    free_map();
 
     return 0;
 }
